ed9bee6c1283c96eaf792eb20b2e2ff791f54be4
strawberry <1556095513@qq.com>
2020-10-31 00:38:58 +0800

    简单写点UI

--- pages/publish/publish.js
+++ pages/publish/publish.js
@@ -1,73 +1,82 @@
+// pages/publish/publish.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    banner_images:['../../images/icon_pink.png',
-    '../../images/icon_pink.png',
-    '../../images/icon_pink.png'],
-    imageURL:'/images/icon_pink.png',
-    book_array:[{'title':'商品标题','desc':'商品描述','price':'2.00','imgurl':'/images/icon_pink.png'},
-    {'title':'商品标题','desc':'商品描述','price':'2.00','imgurl':'/images/icon_pink.png'},
-    {'title':'商品标题','desc':'商品描述','price':'2.00','imgurl':'/images/icon_pink.png'},
-    {'title':'商品标题','desc':'商品描述','price':'2.00','imgurl':'/images/icon_pink.png'},
-    {'title':'商品标题','desc':'商品描述','price':'2.00','imgurl':'/images/icon_pink.png'},
-    {'title':'商品标题','desc':'商品描述','price':'2.00','imgurl':'/images/icon_pink.png'}]
+    select_image_url: ''
   },
+
+  select_pic_action() {
+    var that = this
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success(res) {
+        // tempFilePath可以作为img标签的src属性显示图片
+        const tempFilePaths = res.tempFilePaths
+        that.setData({
+          select_image_url: tempFilePaths[0]
+        })
+      }
+    })
+  },
+
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+
   }
 })
